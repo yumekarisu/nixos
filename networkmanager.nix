2117c8bd58b9a33699649d00e644c3c57e1cf3f5
yumekarisu <zhgalela@protonmail.com>
2025-11-22 13:08:15 +0700

    Revert "Enable iwd backend for nm"
    This reverts commit eced9a456adf464a9f4c499e6a3ad13f89d91c11.

--- networkmanager.nix
+++ networkmanager.nix
@@ -3,18 +3,5 @@
 {
   networking.networkmanager = {
     enable = true;
-    wifi.backend = "iwd";
-    dns = "systemd-resolved";
-  };
-  services.resolved = {
-    enable = true;
-    domains = [ "~." ];
-    dnsovertls = "true";
-    fallbackDns = [
-      "194.242.2.4#base.dns.mullvad.net"
-      "194.242.2.2#dns.mullvad.net"
-      "1.1.1.1#one.one.one.one"
-      "1.0.0.1#one.one.one.one"
-    ];
   };
 }
